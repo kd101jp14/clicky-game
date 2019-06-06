721471e63dd51755540c26a4f5ab5ef43836bce1
kd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-06-06 06:45:04 -0500

    Updated the layout of the application by adding a container to 'Card.js' and give the div for the user's score a class.

--- react-app/src/components/Navbar.js
+++ react-app/src/components/Navbar.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+const styles = {
+  nav: {
+    color: "white",
+    fontSize: "50px"
+  }
+};
+
 function Navbar() {
   return (
-    <div class="navbar navbar-dark bg-dark">
-      <div>
+    <div class="navbar navbar-dark bg-dark" style={styles.nav}>
+      <div class="scoreDiv">
         Score: {} | Top Score: {}
       </div>
     </div>
