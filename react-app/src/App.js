819c6c1e6cfd598b4df0bbb8441d147f42301170
kd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-06-06 06:04:11 -0500

    Created 'App.js'. Imported 'Navbar.js' and 'Card.js' in this file.

---
+++ react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
