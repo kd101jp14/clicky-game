623703a2d47589c81194ce0f52a3474a20e9565e
kd101jp14 <46618187+kd101jp14@users.noreply.github.com>
2019-06-06 05:43:35 -0500

    Created a components directory, in which there is now 'Navbar.js.' The navbar will display the user's score.

---
+++ react-app/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+    return (
+      <div class="navbar navbar-dark bg-dark">
+        <div>Score: {} | Top Score: {}</div>
+      </div>
+    );
+  }
+  
+  export default Navbar;
